cbbb55556846bd13ebc63b3fcb6c8b2c50db0509
Jean-Michel Fortin <jmfortin29@gmail.com>
2022-12-15 20:08:44 -0500

    Updated dataset path

--- fetch_dataset.sh
+++ fetch_dataset.sh
@@ -2,7 +2,7 @@
 # This script downloads the TimberSeg 1.0 dataset. 
 
 sudo apt-get install unzip
-wget -O temp.zip https://md-datasets-cache-zipfiles-prod.s3.eu-west-1.amazonaws.com/y5npsm3gkj-2.zip
+wget -O temp.zip https://prod-dcd-datasets-cache-zipfiles.s3.eu-west-1.amazonaws.com/y5npsm3gkj-2.zip
 unzip temp.zip -d ./data
 rm temp.zip
 mv ./data/y5npsm3gkj*/* ./data
